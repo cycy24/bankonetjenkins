77b9651069a8cbef5a42f477c57f000e171f519b
NawakChevelu <garfield-12@hotmail.fr>
2020-10-15 10:21:36 +0200

    [INIT] Initialisation du projet avec archi SpringBoot

---
+++ src/main/java/com/formation/bankonet/BankonetApplication.java
@@ -0,0 +1,13 @@
+package com.formation.bankonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankonetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formation/bankonet/BankonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.bankonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
